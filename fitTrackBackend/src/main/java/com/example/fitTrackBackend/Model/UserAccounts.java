c86bd1b3d1b0b6648ed63edd2f9fd72a8421bf99
Bogdan Sorinescu <bogdansorinescu@gmail.com>
2025-11-08 17:43:37 +0100

    added table for user accounts JWT

--- fitTrackBackend/src/main/java/com/example/fitTrackBackend/Model/UserAccounts.java
+++ fitTrackBackend/src/main/java/com/example/fitTrackBackend/Model/UserAccounts.java
@@ -3,6 +3,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.sql.Timestamp;
+
 @Entity
 @Getter
 @Setter
@@ -14,5 +16,7 @@ public class UserAccounts {
 
     private String username;
     private String password;
-    private String Email;
+    private Boolean enabled;
+    private String verificationCode;
+    private Timestamp verificationExpired;
 }
