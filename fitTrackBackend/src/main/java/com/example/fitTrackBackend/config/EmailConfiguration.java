890f0c6257265bbfe8b5b18b1f6517b79e78ff64
Bogdan Sorinescu <bogdansorinescu@gmail.com>
2025-11-10 13:10:10 +0100

    added security config file

---
+++ fitTrackBackend/src/main/java/com/example/fitTrackBackend/config/EmailConfiguration.java
@@ -0,0 +1,17 @@
+package com.example.fitTrackBackend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EmailConfiguration {
+
+    @Value("${spring.mail.username}")
+    private String emailUsername;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    
+
+}
