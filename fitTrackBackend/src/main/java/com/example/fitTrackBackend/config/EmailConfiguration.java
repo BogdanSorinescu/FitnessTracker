68c61681e5bfd371631294de383b792a4d99801b
Bogdan Sorinescu <bogdansorinescu@gmail.com>
2025-11-10 13:17:38 +0100

    coded email configuration

--- fitTrackBackend/src/main/java/com/example/fitTrackBackend/config/EmailConfiguration.java
+++ fitTrackBackend/src/main/java/com/example/fitTrackBackend/config/EmailConfiguration.java
@@ -1,7 +1,12 @@
 package com.example.fitTrackBackend.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
 
 @Configuration
 public class EmailConfiguration {
@@ -12,6 +17,22 @@ public class EmailConfiguration {
     @Value("${spring.mail.password}")
     private String password;
 
-    
+    //mail sender for user Auth
+    @Bean
+    public JavaMailSender javaMailSender(){
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername(emailUsername);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
 
 }
